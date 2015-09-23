b0cf52c167f6beef233a98f68ceadfc44b356825
ludos <exorable.ludos@gmail.com>
2015-09-23 10:20:01 -0400

    finished cornerbot bot

--- leboncoin/cornerbot.py
+++ leboncoin/cornerbot.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 """
-# Originally authored by Stephen Sekula (https://hub.polari.us/steve)
-# Distributed under an Apache 2.0 license
-#
-# Modified to post listings for cheap computers on leboncoin in Paris
+    Pump IO Bot for posting leboncoin annonces
+    Python 3
+    MIT License
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+    
+    Fenimore Love
+
+    See Stephen Sekula's weather bot: http://polari.us/dokuwiki/doku.php?id=weatherbot
+
 """
 
-import re
-import sys
-import os
-import time
-
+import re, sys, os, time
 from datetime import datetime
 
 from pypump import PyPump, Client
@@ -19,32 +26,23 @@ from pypump.models.collection import Public
 from pypump.models.person import Person
 from pypump.exception import PyPumpException
 
-from goodcorner import Corner
+from goodcorner import Corner # A script for scraping leboncoin into Dicts
 
+
+# TODO: Format Listing, heading
 """
      Place the credentials below for the pump.io account
-     These can be obtained by using: http://polari.us/dokuwiki/doku.php?id=navierstokes#a_simple_program_to_authenticate_pypump_against_your_pumpio_instance
+     These can be obtained by using: 
+     http://polari.us/dokuwiki/doku.php?id=navierstokes
+ #a_simple_program_to_authenticate_pypump_against_your_pumpio_instance
       or use the get_credentials script for python 3
 """
-
-client_credentials = 
+client_credentials = [secret, password]
 client_tokens = ['puppies', 'kittens'] # These don't go anywhere, but steve had them
-
-# Webfinger for your account
-webfinger = "goodcorners@hotpump.net"
-
-# Important: define a log file that contains a list of activities to which
-# we have already responded.
-#logfile_name = "cornerbot.activity.log"
-
+webfinger = "goodcorners@hotpump.net"# Webfinger for your account
 def simple_verifier(url):
     print('Go to: ' + url)
     return input('Verifier: ') # they will get a code back
-    
-"""
-    Functions for leboncoin
-"""
-
 client = Client(
     webfinger,
     name="CornerBot",
@@ -52,67 +50,55 @@ client = Client(
     key=client_credentials[0], # client key
     secret=client_credentials[1] # client secret
 )
-
-# archive of activities I have already processed
-
-#process_log = open(logfile_name,'a+')
-
-#while 1==1:
 pump = PyPump(
     client=client,
-    #token=client_tokens[0], # the token key NOT NEEDED?
-    #secret=client_tokens[1], # the token secret NOT NEEDED?
     verifier_callback=simple_verifier
 )
 
-
-#posting.send()
-# gather the listings
-# and format the most recent three
-# and add their prices to the note total
 b_set = set() # the B set is TODAYS postings
-listing = ""
-header = ""
 
-#process_log = open(logfile_name,'a+')
+# logfile_name = "cornerbot.activity.log"
+# process_log = open(logfile_name,'a+')
+# process_log.write(blah)
 
 while 99==99:
-    print("\n\ninitiating\n\n")
+    listing = ""
+    header = ""
+    print("\n\nInitiating\n\n")
     timestamp = str(datetime.now())
     header += timestamp + " - Paris - " 
     """
         Gather the Le bon Coin Annonces
         for thinkpads entre 40 et 200 euros
     """
-    # a list of tuples of todays postings
+    # a list of Dicts of todays postings
     corners = Corner.get_recent_corners('http://www.leboncoin.fr/informatique/offres/ile_de_france/?ps=4&pe=8&q=thinkpad&th=1')
     """
         Remove the postings which the
         Cornerbot has already posted
         This requires all sorts of funny business
-        a_set = Most recent scrape
+        a_set = most recent scrape
         b_set = today's scrapes
         c_set = the annonces not yet posted by cornerbot
     """
-    a_set = set() # Add the tuples to a Set
+    a_set = set() # Add the tuples to A set
     for corner in corners:
         tup = tuple(sorted(corner.items()))
         a_set.add(tup)
         
-    c_set = a_set - b_set
-    b_set = a_set
-    print("\nUnique items")
-    print(len(list(c_set)))
-    print("\n Todays itmes")
-    print(len(list(b_set)))    
+    c_set = a_set - b_set # Get the Posting not yet Posted
+    b_set = a_set         # Update the Todays posting set (B)
+    print("\n New items: " , len(list(c_set)))
+    print("\n Total items posted: ", len(list(b_set)))    
+
     """
-        If there are not 0 new postings
-        Post to pump.io
+        Publish the new items to Pump.io
+        Pump publishes in html
     """
-    corners_pub = [dict(x) for x in list(c_set)] # sweet combobulator!!
-    
-    if corners_pub: # if there exists new items (from a - b )
-        for corner in corners_pub:
+    corners_pub = map(dict, c_set) # map takes a function and an iterable 
+    print(type(corners_pub))   
+    if len(list(c_set)) is not 0: # if there exists new items (from a - b )
+        for corner in corners_pub: # TODO: https://pyformat.info/
             # Set up HTML Styled Posting for Pump.io
             listing += "<table><tr><td><img src=" + corner['image'] + " width=100px height=auto/></td><td><b><a href=" + corner['url'] + ">"+ corner['titre'] + "</a></b><br><a href=" + corner['url'] + " >Annonce</a> " + corner['location']+" pour " + corner['prix'] + "<br>" + corner['date'] +"</td></tr></table>" 
             listing += "<br>"
@@ -121,37 +107,14 @@ while 99==99:
             print("...   "+ corner['titre'])
         posting = pump.Note(listing, header)
         posting.to = pump.Person("polypmer@microca.st")
-        #posting.send()
-        print("posting " +timestamp)
+        posting.send()
+        print("Posting " +timestamp)
     else:
-        print("no new corners\n\n\n")
+        print("No new corners\n\n\n")    
     # Finished Post
-    listing = ""
-    header = ""
     print("\n\nSleep Now")
-    time.sleep(3)
+    time.sleep(7000) # 3600 Seconds = Hour
 
 #process_log.close()
-    
-    """
-    for x in range(2):
-        listing += "<table><tr><td><img src=" + corners[x]['image'] + " width=100px height=auto/></td><td><b>" + corners[x]['titre'] + "</b><br><a href=" + corners[x]['url'] + " >Annonce</a> " + corners[x]['location']+" pour " + corners[x]['prix'] + "<br>" + corners[x]['date'] +"</td></tr></table>" 
-        listing += "<br>" 
-        # "<br><b>" + corner['titre'] + "</b><br><img src=" + corner['image'] + " width=30px height=auto/><br><a href=" + corner['url'] + " >Annonce</a> " + corner['location']+" pour " + corner['prix'] + "<br><br>"  
-        
-        
-            new_corners = [] # can't remove from list I am iterating over.
-    if todays_corners:
-        for item in corners:
-            if item not in corners:
-                new_corners.append(item)
-                print("Unique")
-                continue
-            else:
-                print("unique\n") 
-                print("...   "+ item['titre'])
-                continue
-        
-        """ 
 
 
--- leboncoin/goodcorner.py
+++ leboncoin/goodcorner.py
@@ -8,12 +8,13 @@
     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
     THE SOFTWARE.
+    
+    Fenimore Love
 """
 
 import sys, requests, os, re, datetime
 from bs4 import BeautifulSoup
 
-# This is where the files are hosted
 sample_url= "http://www.leboncoin.fr/informatique/offres/ile_de_france/?ps=4&pe=8&q=thinkpad&th=1"
 """
     Designed for computers, but could be used for anything I suppose with the right link
@@ -24,7 +25,6 @@ sample_url= "http://www.leboncoin.fr/informatique/offres/ile_de_france/?ps=4&pe=
     - q = keywords (can take and/or operators, and more)
 """
 
-
 class Corner(object):
     title = ""
     url = ""
@@ -34,7 +34,8 @@ class Corner(object):
     category = ""
     image = ""
 		
-    #constructor the de-structor!!  
+    # constructor the de-structor!!  
+    # I haven't yet actually automated one such method though
     def __init__(self, title, url, price, location, date, category, image):
         self.title = title
         self.url = url
@@ -47,54 +48,55 @@ class Corner(object):
     #the stringifing printer.... Python is so pretty
     def __str__(self):
         return " Titre: %s \n URL: %s \n Prix: %s \n" % (self.title, self.url, self.price)
-
     def get_corners(url):
-        #corners = []
         corner_dicts = scrape_listings(url)
-        #for corner_dict in corner_dicts:
-        #    corner = Corner(corner_dict['titre'], corner_dict['url'], corner_dict['prix'], corner_dict['location'], corner_dict['date'], corner_dict['category'],corner_dict['image'])
-        #corners.append(corner)
         return corner_dicts
     def get_recent_corners(url):
         corners = scrape_listings(url)
         recents = get_recent(corners)
         return recents
-
-
+        
+"""
+    This is the bread and butter of the program. Scrape Listing
+    Constructs a dict for each of the from page of a search,
+    given a url. This can then be put into the Corner object.
+    Or just handled as a dict.    
+"""
 
 def scrape_listings(url):
-    computers = []# define set of listings
+    computers = []
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
-    ordinateurs = soup.find_all("div", {"class":"lbc"}) # These are the unsifted listings
-    x = 0 # Counter, Why not?
+    ordinateurs = soup.find_all("div", {"class":"lbc"})
+    # x = 0 # Counter, Why not?
+    """
+        The ordinateur has english attributes
+        et le computer a les francais
+        Duh
+        Getting images can be tricky, if they don't exist.
+        Le Bon Coin uses all kinds of weird newline and space oddities
+        to mess with us. Hence the plentiful (but necessary!)
+        replace() and strip() functions 
+    """
+    
     for ordinateur in ordinateurs:
         url = ordinateur.find_parent('a')['href']
-        #images need to be something like, if in class=image div, find img element 
-        image_url = "" #ordinateur.find("img")['src'].text
         date = ordinateur.find("div", {'class':'date'}).text.replace('\n'," ").strip()
         details = ordinateur.find('div', {'class':'detail'})
         title = str(details.find('h2', {'class':'title'}).string).replace('\n', '').strip()
         category = str(details.find('div', {'class':'category'}).string).replace('\n', '').strip()
         placement = str(details.find('div', {'class':'placement'}).string).replace('\n', '').strip().replace('                                                                                            /                                                                                                                            	                                        '," ")
         price =  str(details.find('div', {'class':'price'}).string).replace('\n', '').strip()
-        ## Images are a bit more complicated:
         try:
             image_div = ordinateur.find('div', {'class':'image'})
             image_nb = image_div.find('div', {'class':'image-and-nb'})
             image_url = image_nb.find('img')['src']
         except:
             image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/200px-No_image_available.svg.png" # No-image image        
-        # The ordinateur has english attributes
-        # et le computer avais les francais
         computer = {'url':url, 'titre': title, 'prix': price,'location': placement, 'image': image_url, 'date':date, 'category': category }
-        #computer = tuple(sorted(computer.items()))
-        """Send Dicts
-        """
-        computers.append(computer)
-        #x += 1
-        #print(x)
-    return computers #returns a list of tuples
+        computers.append(computer) # A list of Dicts
+        #x += 1 # Counter, why not?
+    return computers
     
 """
     Computer = {
@@ -102,15 +104,13 @@ def scrape_listings(url):
                }
 """
  
-def get_recent(computers): # get the computers listed TODAY
+def get_recent(computers): # TODAYS listings
     recent_computers = []
     for computer in computers:
-        computer = dict(computer)
         today = str(datetime.datetime.now().day) + '-' + str(datetime.datetime.now().month)
-        c = re.compile('Aujourd\'hui')
+        c = re.compile('Aujourd\'hui') # Regex match, this is because of Le Bon Coin's format
         if c.match(computer['date']):
             computer['date'] = computer['date'].replace('Aujourd\'hui', today)
-            #computer = tuple(sorted(computer))
             recent_computers.append(computer)
     return recent_computers
 
